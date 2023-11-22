895b09e70c970935cdfc5e103b08ffc67d7a5d4b
V L Kumar <zenithkumar2016@gmail.com>
2023-11-22 23:24:48 +0530

    refactor: proper comments added in index file

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,11 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-// Currently disabled StrictMode it's rendering 2 times in develop mode
+// Disable StrictMode When we are develop mode because it's rendering 2 times in develop mode
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
       <App />
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
